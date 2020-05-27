8abdd7fec20ff21cb41b2ed9baea061f502c9ecd
Nivolving <nivethapartheeban@gmail.com>
2020-05-27 12:34:10 -0700

    Updated the projects with github links

--- script.js
+++
@@ -1,9 +0,0 @@
-$('#weatherDashboard').delegate("div","click", function(){
-    alert("hey its working");
-    event.preventDefault();
-   window.location = $(this).find("a").attr("https://nivolving.github.io/WeatherDashboard/");
- 
-   
-});
-
-//console.log("hello");
